b8cc4936709f6ed608c0338b8c10b0438e45c068
Gespinosa <gespinosa01@icloud.com>
2024-12-13 17:37:51 -0700

    Mostrando muchas cosas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoItem } from './TodoItem';
+import { TodoCounter } from './TodoCounter';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoList } from './TodoList';
+import { TodoSearch } from './TodoSearch';
+
+const defaultTodos = [
+  {text: "Cortar cebolla", completed: true},
+  {text: "Aprender maps", completed: true},
+  {text: "Domar React", completed: true},
+  {text: "Llorar con la llorona", completed: true},
+  {text: "Mirar platzi", completed: true}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter completed={1} total={4} />
+      <TodoSearch/>
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem key={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton/>
+    </>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,6 @@
+function TodoSearch (){
+  return (
+    <input placeholder="Hola"/>
+  );
+}
+ export { TodoSearch };
